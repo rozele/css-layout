35901bb9cf020df1d6fc7fc4c322110aa1053d31
Armin Sander <armin@replicator.org>
2015-02-24 10:25:42 +0100

    csharp: add nuget packaging support

--- src/csharp/Facebook.CSSLayout/Properties/AssemblyInfo.cs
+++ src/csharp/Facebook.CSSLayout/Properties/AssemblyInfo.cs
@@ -1,32 +1,18 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Facebook.CSSLayout")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A subset of CSS's flexbox layout algorithm and box model. Note that this is an unofficial port of the Facebook's css-layout open source project to .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Armin Sander")]
 [assembly: AssemblyProduct("Facebook.CSSLayout")]
 [assembly: AssemblyCopyright("Copyright © 2015 Facebook")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("Facebook.CSSLayout.Tests")]
